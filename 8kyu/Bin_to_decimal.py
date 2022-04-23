684032e4519c9a50a46071901722e17cf8552ad5
rkjar-work <rkjar@yandex.ru>
2022-04-23 12:56:30 +0500

    Added new solutions

---
+++ 8kyu/Bin_to_decimal.py
@@ -0,0 +1,11 @@
+"""
+Bin to Decimal
+
+Complete the function which converts a binary number (given as a string) to a decimal number.
+
+https://www.codewars.com/kata/57a5c31ce298a7e6b7000334
+"""
+
+
+def bin_to_decimal(inp):
+    return int(inp, 2)
